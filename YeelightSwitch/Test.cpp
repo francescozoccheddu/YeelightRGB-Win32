dca5a46936262246511c07eb46ee2b7788c32cd7
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2018-05-10 23:33:53 +0200

    Button

--- YeelightSwitch/Test.cpp
+++ YeelightSwitch/Test.cpp
@@ -4,10 +4,47 @@
 #include <winerror.h>
 #include "resource.h"
 
+#define IDC_BUTTON   108
+
+const int g_ColorCount = 4;
+
+void PrintError (LPCTSTR _caption);
+
 LRESULT CALLBACK MainWinProc (HWND _hwnd, UINT _msg, WPARAM _wparam, LPARAM _lparam)
 {
 	switch (_msg)
 	{
+		case WM_GETMINMAXINFO:
+		{
+			LPMINMAXINFO lpMMI = (LPMINMAXINFO)_lparam;
+			lpMMI->ptMinTrackSize.x = 300;
+			lpMMI->ptMinTrackSize.y = 300;
+			lpMMI->ptMaxTrackSize.x = 900;
+			lpMMI->ptMaxTrackSize.x = 700;
+		}
+		break;
+		case WM_CREATE:
+		{
+			HINSTANCE hInstance = (HINSTANCE)GetWindowLong (_hwnd, GWLP_HINSTANCE);
+			for (int b = 0; b < g_ColorCount; b++)
+			{
+				HWND hwndButton = CreateWindow (TEXT("BUTTON"), TEXT(""),
+					WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+					0, 0, 0, 0, _hwnd, (HMENU)(IDC_BUTTON + b), hInstance, NULL); 
+			}
+		}
+		break;
+		case WM_SIZE:
+		{
+			RECT rcClient;
+			GetClientRect (_hwnd, &rcClient);
+			for (int b = 0; b < g_ColorCount; b++)
+			{
+				HWND hEdit = GetDlgItem (_hwnd, (IDC_BUTTON + b));
+				SetWindowPos (hEdit, NULL, 0, 0, rcClient.right, rcClient.bottom, SWP_NOZORDER);
+			}
+		}
+		break;
 		case WM_CLOSE:
 			DestroyWindow (_hwnd);
 			break;
@@ -20,21 +57,21 @@ LRESULT CALLBACK MainWinProc (HWND _hwnd, UINT _msg, WPARAM _wparam, LPARAM _lpa
 	return 0;
 }
 
-void PrintError (LPCTSTR caption)
+void PrintError (LPCTSTR _caption)
 {
 	DWORD error = GetLastError ();
 	LPTSTR messagePtr;
 	if (FormatMessage (FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, 0, GetLastError (), 0, (LPTSTR)&messagePtr, 0, NULL) > 0)
 	{
-		MessageBox (NULL, messagePtr, caption, MB_OK | MB_ICONERROR);
+		MessageBox (NULL, messagePtr, _caption, MB_OK | MB_ICONERROR);
 		LocalFree (messagePtr);
 	}
 }
 
-LPCTSTR TryLoadString (HINSTANCE _hInstance, UINT id)
+LPCTSTR TryLoadString (HINSTANCE _hInstance, UINT _id)
 {
 	LPCTSTR ptr;
-	if (LoadString (_hInstance, id, (LPTSTR)&ptr, 0) <= 0)
+	if (LoadString (_hInstance, _id, (LPTSTR)&ptr, 0) <= 0)
 	{
 		ptr = TEXT("");
 	}
@@ -66,7 +103,7 @@ int CALLBACK WinMain (HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPSTR _cmd
 		return 0;
 	}
 
-	HWND window = CreateWindow (mainClassAtom, TryLoadString(_hInstance, IDS_TITLE), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 400, NULL, NULL, _hInstance, NULL);
+	HWND window = CreateWindow (MAKEINTATOM(mainClassAtom), TryLoadString(_hInstance, IDS_TITLE), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 400, NULL, NULL, _hInstance, NULL);
 	if (window == NULL)
 	{
 		PrintError (TryLoadString (_hInstance, IDS_ERROR_CREATE_MAIN_WINDOW_CAPTION));
@@ -75,11 +112,13 @@ int CALLBACK WinMain (HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPSTR _cmd
 
 	ShowWindow (window, _cmdShow);
 
+
+
 	MSG msg;
-	BOOL mRes;
-	while ((mRes = GetMessage (&msg, NULL, 0, 0)) != 0)
+	BOOL bRes;
+	while ((bRes = GetMessage (&msg, NULL, 0, 0)) != 0)
 	{
-		if (mRes == -1)
+		if (bRes == -1)
 		{
 			PrintError (TryLoadString (_hInstance, IDS_ERROR_MESSAGE_LOOP_CAPTION));
 			break;
